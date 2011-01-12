0aac0efe09ba1518d122fca9db906d90c42872ad
info@matisen.dk <info@matisen.dk@c2f9df8b-a7b7-652c-7386-7904da534038>
2011-01-12 12:41:18 +0000

    Initial commit

---
+++ trunk/Complete_System/project/_impact.cmd
@@ -0,0 +1,26 @@
+loadProjectFile -file "C:\Documents and Settings\s000536\Desktop\complete_system/complete_system.ipf"
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+Program -p 3 
+saveProjectFile -file "C:/Documents and Settings/s000536/Desktop/complete_system/complete_system.ipf"
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
